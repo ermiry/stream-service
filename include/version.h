fa884b91b3287a795d6c84af0a1cebbf8b9fa25d
ermiry <erick.salas@ermiry.com>
2021-08-22 17:08:06 -0500

    Version 0.1

--- include/version.h
+++ include/version.h
@@ -3,8 +3,8 @@
 
 #define STREAM_VERSION			"0.1"
 #define STREAM_VERSION_NAME		"Version 0.1"
-#define STREAM_VERSION_DATE		"17/08/2021"
-#define STREAM_VERSION_TIME		"23:11 CST"
+#define STREAM_VERSION_DATE		"22/08/2021"
+#define STREAM_VERSION_TIME		"17:07 CST"
 #define STREAM_VERSION_AUTHOR	"Erick Salas"
 
 // print full stream version information
