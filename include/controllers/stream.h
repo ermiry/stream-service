b6fd36d4cc10f053a9d3ba38f62bf284effdce61
ermiry <erick.salas@ermiry.com>
2021-08-30 14:35:07 -0500

    Added controllers to handle images & videos

--- include/controllers/stream.h
+++ include/controllers/stream.h
@@ -3,18 +3,27 @@
 
 #include <bson/bson.h>
 
+#include "errors.h"
+
+struct _HttpReceive;
+
 extern unsigned int service_videos_get_all (
 	char **json, size_t *json_len
 );
 
 extern ServiceError service_video_get_image (
-	const char *video_id, const char *filename
+	const char *video_id, char *filename
 );
 
 extern unsigned int service_video_get_by_id_to_json (
 	const char *video_id,
 	const bson_t *query_opts,
 	char **json, size_t *json_len
+);
+
+extern ServiceError service_video_stream (
+	const struct _HttpReceive *http_receive,
+	const char *video_id, const char *range
 );
 
 #endif
--- src/controllers/stream.c
+++ src/controllers/stream.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-#include <cerver/files.h>
+#include <cerver/http/http.h>
+#include <cerver/http/response.h>
 
 #include <cerver/utils/log.h>
 
@@ -18,8 +21,8 @@ unsigned int service_videos_get_all (
 
 }
 
-ServiceError service_video_get_image (
-	const char *video_id, const char *filename
+static ServiceError service_video_get_by_id (
+	const char *video_id, Video **found
 ) {
 
 	ServiceError error = SERVICE_ERROR_NONE;
@@ -30,20 +33,40 @@ ServiceError service_video_get_image (
 		bson_oid_init_from_string (&video_oid, video_id);
 
 		if (!video_get_by_oid (video, &video_oid, NULL)) {
-			(void) strncpy (filename, video->filename, VIDEO_IMAGE_SIZE - 1);
+			*found = video;
 		}
 
 		else {
 			cerver_log_error ("Failed to get video %s!", video_id);
 
 			error = SERVICE_ERROR_NOT_FOUND;
-		}
 
-		video_delete (video);
+			video_delete (video);
+		}
 	}
 
 	else {
 		error = SERVICE_ERROR_SERVER_ERROR;
+	}
+
+	return error;
+
+}
+
+ServiceError service_video_get_image (
+	const char *video_id, char *filename
+) {
+
+	Video *video = NULL;
+
+	ServiceError error = service_video_get_by_id (
+		video_id, &video
+	);
+
+	if (error == SERVICE_ERROR_NONE) {
+		(void) strncpy (filename, video->image, VIDEO_IMAGE_SIZE - 1);
+
+		video_delete (video);
 	}
 
 	return error;
@@ -70,5 +93,28 @@ unsigned int service_video_get_by_id_to_json (
 	}
 
 	return retval;
+
+}
+
+ServiceError service_video_stream (
+	const HttpReceive *http_receive,
+	const char *video_id, const char *range
+) {
+
+	Video *video = NULL;
+
+	ServiceError error = service_video_get_by_id (
+		video_id, &video
+	);
+
+	if (error == SERVICE_ERROR_NONE) {
+		http_response_handle_video (
+			http_receive, video->filename
+		);
+
+		video_delete (video);
+	}
+
+	return error;
 
 }
